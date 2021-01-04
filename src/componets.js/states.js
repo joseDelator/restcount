b20f9c06a64a2897d03630259aba9c33aea12993
joseDelator <49530849+joseDelator@users.noreply.github.com>
2021-01-04 09:19:12 -0800

    spinner

--- src/componets.js/states.js
+++ src/componets.js/states.js
@@ -1,19 +1,29 @@
 import React, {Component, } from 'react';
+import Spiner from '../spiner.js';
 import {ProductConsumer} from  './context.js'; 
 import Stat from './Stat'
+
 class Stats extends Component{
- 
+
   
  render(){
+    
      return(
+         
          <ProductConsumer>
+
              {value=>{
                  const {player}= value;
                  console.log(value);
+                 if(player.lenght ===0 || player === undefined){
+                     return <Spiner/>;
+                 }
+                 else{
                  return (
 
                      <React.Fragment>
                         <div className = "container">
+                            
                            {player.map(item =>(
                                <Stat  key= {item.name} player={item}/>
                            ))}
@@ -22,9 +32,10 @@ class Stats extends Component{
 
                  )
                
-             }}
+             }}}
          </ProductConsumer>
      )
  }
 }
+
 export default Stats
---
+++ src/spiner.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import "../src/spinner.css"
+export default class Spiner extends Component {
+    render() {
+        return (
+            <div className="loader"/>
+        )
+    }
+}
